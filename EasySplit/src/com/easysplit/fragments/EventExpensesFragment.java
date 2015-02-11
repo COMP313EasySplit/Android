468133d19067946cf538f92cd382d81cb5fa63f2
Kathleen <kathleen.wilsonn@gmail.com>
2015-02-10 21:44:49 -0500

    Event Expense Tab UI
    Event Expense Tab UI with listview details xml file

--- EasySplit/src/com/easysplit/fragments/EventExpensesFragment.java
+++ EasySplit/src/com/easysplit/fragments/EventExpensesFragment.java
@@ -14,8 +14,8 @@ public class EventExpensesFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.event_expense_fragment, container, false);
-		TextView textview = (TextView) view.findViewById(R.id.expense_tabtext);
-		textview.setText("tab 2");
+		//TextView textview = (TextView) view.findViewById(R.id.expense_tabtext);
+		//textview.setText("tab 2");
 		
 		return view;
 	}
