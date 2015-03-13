fc5431e18e7a3d51625032ff61c34a6dfe09ca25
JohnnyHu9 <r17d09@gmail.com>
2015-03-12 23:34:40 -0400

    add take photo in new expense
    call default camera application to take photo and save to photos. a
    preview is generated on the screen.

--- EasySplit/src/com/easysplit/mainview/NewExpense.java
+++ EasySplit/src/com/easysplit/mainview/NewExpense.java
@@ -1,12 +1,23 @@
 package com.easysplit.mainview;
 
+import java.io.File;
+
 import com.example.easysplit.R;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class NewExpense extends Activity {
 
@@ -34,4 +45,42 @@ public class NewExpense extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
+	
+	private static final int TAKE_PICTURE = 1;    
+	private Uri imageUri;
+	public void btnNexPhoto(View v)
+	{
+	    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+	    File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+	    intent.putExtra(MediaStore.EXTRA_OUTPUT,
+	            Uri.fromFile(photo));
+	    imageUri = Uri.fromFile(photo);
+	    startActivityForResult(intent, TAKE_PICTURE);
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    switch (requestCode) {
+	    case TAKE_PICTURE:
+	        if (resultCode == Activity.RESULT_OK) {
+	            Uri selectedImage = imageUri;
+	            getContentResolver().notifyChange(selectedImage, null);
+	            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+	            ContentResolver cr = getContentResolver();
+	            Bitmap bitmap;
+	            try {
+	                 bitmap = android.provider.MediaStore.Images.Media
+	                 .getBitmap(cr, selectedImage);
+
+	                imageView.setImageBitmap(bitmap);
+	                Toast.makeText(this, selectedImage.toString(),
+	                        Toast.LENGTH_LONG).show();
+	            } catch (Exception e) {
+	                Toast.makeText(this, "Failed to load", Toast.LENGTH_LONG)
+	                        .show();
+	                Log.e("Camera", e.toString());
+	            }
+	        }
+	    }
+	}	
 }
